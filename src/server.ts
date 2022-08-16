21350c54ecc814f92a07bf90436af7f22215582c
GranTY <grantygames@gmail.com>
2022-08-16 17:41:51 -0400

    Controllers

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
+
 
 const server = express();
 
@@ -13,4 +15,12 @@ server.engine('mustache', mustache());
 server.use(express.static(path.join(__dirname, '../public')));
 
 // Rotas
+server.use(mainRoutes);
+
+//Status 404
+server.use((req, res) => {
+    res.send('pagina não encontrada')
+})
+
+//Porta do servidor
 server.listen(process.env.PORT);
