ad7920eaf6864ab807dacd846129f5201f6a9ec4
GranTY <grantygames@gmail.com>
2022-08-16 23:09:58 -0400

    proibir a pesquisa de algo nulo.

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -7,6 +7,11 @@ export const search = (req: Request, res: Response) => {
 
     let query: string = req.query.q as string;
 
+    if(!query){
+        res.redirect('/')
+        return;
+    }
+
     let list = Pet.getFromName(query);
 
     res.render('pages/page', {
